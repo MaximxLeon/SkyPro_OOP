053d3b5f5949d0b3be678679ad7115b574efd396
maxim_rozhnev <kaster3190@mail.ru>
2026-05-14 15:34:05 +0700

    Add abstract class add new class

--- src/product.py
+++ src/product.py
@@ -1,50 +1,69 @@
-class Product:
-    def __init__(
-        self,
-        name: str,
-        description: str,
-        price: float,
-        quantity: int
-    ):
+from abc import ABC, abstractmethod
+
+
+class BaseProduct(ABC):
+    @abstractmethod
+    def get_price(self):
+        pass
+
+    @abstractmethod
+    def get_description(self):
+        pass
+
+
+class MixinProduct:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        print(f"Создан объект: {self.__class__.__name__} -> {self}")
+
+
+class Product(MixinProduct, BaseProduct):
+    def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
         self.__price = price
         self.quantity = quantity
 
+        super().__init__()
+
+    def get_price(self):
+        return self.__price
+
+    def get_description(self):
+        return self.description
+
     def __add__(self, other):
         if isinstance(other, Product):
             return (
-                (self.price * self.quantity) +
-                (other.price * other.quantity)
+                (self.__price * self.quantity) +
+                (other.__price * other.quantity)
             )
         raise TypeError("Можно складывать только объекты Product")
 
     def __str__(self):
-        return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
+        return f"{self.name}, {self.__price} руб. Остаток: {self.quantity} шт."
 
     @property
     def price(self):
-        """Геттер для получения цены продукта."""
         return self.__price
 
     @price.setter
     def price(self, new_price: float):
-        """Сеттер для установки новой цены продукта."""
         if new_price <= 0:
             print("Цена не должна быть нулевая или отрицательная")
         else:
             self.__price = new_price
 
-    @classmethod
-    def new_product(cls, product_data: dict):
-        """Создаёт новый продукт."""
-
-        return cls(
-            name=product_data["name"],
-            description=product_data["description"],
-            price=product_data["price"],
-            quantity=product_data["quantity"]
+    def __repr__(self):
+        return (
+            f"Product('{self.name}',\
+            '{self.description}', {self.__price}, {self.quantity})"
         )
 
-    def __repr__(self):
-        return f"Product('{self.name}', {self.price}, {self.quantity})"
+
+class Smartphone(Product):
+    pass
+
+
+class Grass(Product):
+    pass
